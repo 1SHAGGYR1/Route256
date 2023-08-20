415d7d588538c47b68991dfdcc234e99f0f099c4
e.Merkulov <merkulov.e@medlinx.online>
2023-08-20 14:00:24 +0300

    master Added report task

---
+++ Report/Program.cs
@@ -0,0 +1,25 @@
+﻿var testsCount = int.Parse(Console.ReadLine());
+for (var i = 0; i < testsCount; i++)
+{
+    var metReports = new HashSet<int>();
+    Console.ReadLine();
+    var match = true;
+    var previous = 0;
+    foreach (var report in Console.ReadLine().Split(' ').Select(int.Parse))
+    {
+        if (previous != report)
+        {
+            metReports.Add(previous);
+            if (metReports.Contains(report))
+            {
+                match = false;
+                break;
+            }
+        }
+
+        previous = report;
+    }
+
+    Console.WriteLine(match ? "YES" : "NO");
+    metReports.Clear();
+}
