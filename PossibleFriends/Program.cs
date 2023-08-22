f7e8b154657aa148f198ab088cc7b4cff9c5e136
e.Merkulov <merkulov.e@medlinx.online>
2023-08-23 00:29:53 +0300

    master Transfer to PC

--- PossibleFriends/Program.cs
+++ PossibleFriends/Program.cs
@@ -1,22 +1,21 @@
 ﻿var input = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
 var usersCount = input[0];
-var pairsCount = input[1];
-var friendsGraph = new int[usersCount, usersCount];
+var friendsLists = Enumerable.Range(1, usersCount).Select(_ => new List<int>(5)).ToArray();
 
+var pairsCount = input[1];
 for (var i = 0; i < pairsCount; i++)
 {
     var pair = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-    friendsGraph[pair[0] - 1, pair[1] - 1] = -1;
+    friendsLists[pair[0] - 1].Add(pair[1] - 1);
+    friendsLists[pair[1] - 1].Add(pair[0] - 1);
 }
 
+var possibleFriends = new Dictionary<int, Dictionary<int, int>>(usersCount);
 for (var i = 0; i < usersCount; i++)
 {
-    var friendsOfI = new List<int>();
-    for (var j = 0; j < usersCount; j++)
+    foreach (var friend in friendsLists[i])
     {
-        if (friendsGraph[i, j] == -1)
-        {
-            friendsOfI.Add(j);
-        }
+        if (friendsLists[friend].Contains())
     }
+    var friendsOfI = new List<int>();
 }
