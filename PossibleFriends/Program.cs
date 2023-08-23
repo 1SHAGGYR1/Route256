ca19d191742fd0a5c042f342e3d654e2883e0846
Evgenii <zhenya.shaggyr@gmail.com>
2023-08-23 03:03:27 +0300

    master Finished Possible Friends

--- PossibleFriends/Program.cs
+++ PossibleFriends/Program.cs
@@ -15,7 +15,50 @@ for (var i = 0; i < usersCount; i++)
 {
     foreach (var friend in friendsLists[i])
     {
-        if (friendsLists[friend].Contains())
+        foreach (var other in friendsLists[i].Except(new List<int>{ friend }))
+        {
+            if (!friendsLists[friend].Contains(other))
+            {
+                if (!possibleFriends.TryGetValue(friend, out var userPossibleFriends))
+                {
+                    userPossibleFriends = new Dictionary<int, int>();
+                    possibleFriends.Add(friend, userPossibleFriends);
+                }
+                if (userPossibleFriends.ContainsKey(other))
+                {
+                    userPossibleFriends[other]++;
+                }
+                else
+                {
+                    userPossibleFriends[other] = 1;
+                }
+            }
+        }
     }
-    var friendsOfI = new List<int>();
+}
+
+for (var i = 0; i < usersCount; i++)
+{
+    if (possibleFriends.TryGetValue(i, out var resultPossibleFriends))
+    {
+        var maxValue = 0;
+        var result = new SortedSet<int>();
+        foreach (var pair in resultPossibleFriends)
+        {
+            if (pair.Value > maxValue)
+            {
+                result.Clear();
+                result.Add(pair.Key + 1);
+                maxValue = pair.Value;
+            } else if (pair.Value == maxValue)
+            {
+                result.Add(pair.Key + 1);
+            }
+        }
+        Console.WriteLine(string.Join(' ', result));
+    }
+    else
+    {
+        Console.WriteLine("0");
+    }
 }
